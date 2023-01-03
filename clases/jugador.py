32cd9f4c0966c278fff94747296a1bd2a5235e6a
Paul Muabdib - Miguel Morera Belenguer <paul.muabdib.usul@gmail.com>
2023-01-04 00:16:09 +0100

    Implementación clase jugador.

---
+++ clases/jugador.py
@@ -0,0 +1,24 @@
+import pygame
+
+class Nave(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.imagenNave = pygame.image.load("imagenes/Nave.png") # Cargamos la imagen
+        self.rect = self.imagenNave.get_rect() # Calculamos el rectángulo de la imagen
+        # Posición inicial de la nave
+        self.rect.centerx = 200
+        self.rect.centery = 750
+        # Otros atributos
+        self.velocidad = 10
+        self.vida = True
+        self.listaDisparo = []
+    def mover(self):
+        if self.vida == True:
+            if self.rect.left <= 0:
+                self.rect.left = 0
+            elif self.rect.right > 364:
+                self.rect.right = 364
+    def disparar(self):
+        print('Disparando...')
+    def dibujar(self, superficie):
+        superficie.blit(self.imagenNave, self.rect)
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import pygame, sys
 from pygame.locals import *
 
+from clases import jugador
+
 # DEFINICIÓN DE VALRIABLES Y CONSTANTES
 #######################################
 ALTO_VENTANA = 800 # Alto de la ventana donde cargaremos el juego
 ANCHO_VENTANA = 400 # Ancho de la ventana donde cargaremos el juego
+
+jugando = True
+
 
 # FUNCIÓN PRINCIPAL
 ###################
@@ -14,15 +19,27 @@ def meteoritos():
     ventana = pygame.display.set_mode((ANCHO_VENTANA, ALTO_VENTANA))
     # Cargamos el fondo
     fondo = pygame.image.load("imagenes/FondoAsteroides.png")
-    ventana.blit(fondo,(0,0))
     # Titulo
     pygame.display.set_caption('Asteroides')
+    # Creamos objeto jugador
+    nave = jugador.Nave()
+
     # Ciclo del juego
     while True:
+        ventana.blit(fondo, (0, 0)) # Dibujamos fondo
+        nave.dibujar(ventana)  # Dibujamos nave
+        nave.mover()
         for evento in pygame.event.get():
             if evento.type == QUIT:
                 pygame.quit()
                 sys.exit()
+            elif evento.type == pygame.KEYDOWN:
+                if evento.key == K_LEFT:
+                    nave.rect.left -= nave.velocidad
+                elif evento.key == K_RIGHT:
+                    nave.rect.right += nave.velocidad
+                elif evento.key == K_SPACE:
+                    nave.disparar()
         pygame.display.update()
 
 
