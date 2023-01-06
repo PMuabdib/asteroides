0f222f5ea8ae525e48f2b51beaa0c1fd48751aa7
Paul Muabdib - Miguel Morera Belenguer <paul.muabdib.usul@gmail.com>
2023-01-06 20:43:13 +0100

    Implementación resto del juego.

--- clases/jugador.py
+++ clases/jugador.py
@@ -5,6 +5,7 @@ class Nave(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.imagenNave = pygame.image.load("imagenes/Nave.png") # Cargamos la imagen
+        self.imagenExplota = pygame.image.load("imagenes/explosion.png")
         self.rect = self.imagenNave.get_rect() # Calculamos el rectángulo de la imagen
         # Posición inicial de la nave
         self.rect.centerx = 200
@@ -13,6 +14,7 @@ class Nave(pygame.sprite.Sprite):
         self.velocidad = 10
         self.vida = True
         self.listaDisparo = []
+        self. sonidoDisparo = pygame.mixer.Sound("sonidos/458669.ogg")
     def mover(self):
         if self.vida == True:
             if self.rect.left <= 0:
@@ -20,7 +22,12 @@ class Nave(pygame.sprite.Sprite):
             elif self.rect.right > 364:
                 self.rect.right = 364
     def disparar(self, x, y):
-        misil = disparo.Misil(x, y)
-        self.listaDisparo.append(misil)
+        if self.vida == True:
+            misil = disparo.Misil(x, y)
+            self.listaDisparo.append(misil)
+            self.sonidoDisparo.play()
     def dibujar(self, superficie):
-        superficie.blit(self.imagenNave, self.rect)
+        if self.vida == True:
+            superficie.blit(self.imagenNave, self.rect)
+        else:
+            superficie.blit(self.imagenExplota, self.rect)
--- main.py
+++ main.py
@@ -9,7 +9,13 @@ from clases import asteroide
 #######################################
 ALTO_VENTANA = 800 # Alto de la ventana donde cargaremos el juego
 ANCHO_VENTANA = 400 # Ancho de la ventana donde cargaremos el juego
+colorFuente = (255,255,255) # Color para la fuente del marcador
+
 listaAsteroides = []
+
+puntos = 0 # Variable para la puntuación del juego
+
+
 jugando = True
 
 
@@ -22,6 +28,13 @@ def cargarAsteroides(x, y):
 
 # FUNCIÓN PRINCIPAL
 ###################
+def gameOver():
+    global jugando
+    jugando = False
+    for asteroide in listaAsteroides:
+        listaAsteroides.remove(asteroide)
+
+
 def meteoritos():
     pygame.init()
     # Cargamos la ventana
@@ -30,17 +43,32 @@ def meteoritos():
     fondo = pygame.image.load("imagenes/FondoAsteroides.png")
     # Titulo
     pygame.display.set_caption('Asteroides')
+    # Música de fondo
+    pygame.mixer.music.load("sonidos/388880.wav")
+    pygame.mixer.music.play(-1) # Reproducir en bucle infinito '-1'
+    # Sonido Colisión
+    sonidoColision =  pygame.mixer.Sound("sonidos/394128.wav")
+    # Sonido destruccion asteroide
+    sonidoDestruccion = pygame.mixer.Sound("sonidos/472061.wav")
+    # Fuente para el marcador
+    fuenteMarcador = pygame.font.SysFont("Arial", 20)
+    fuenteGameOver = pygame.font.SysFont("Arial", 40)
     # Creamos objeto jugador
     nave = jugador.Nave()
+    # Variable contador de tiempo
     contador = 0
 
     # Ciclo del juego
     while True:
         ventana.blit(fondo, (0, 0)) # Dibujamos fondo
         nave.dibujar(ventana)  # Dibujamos nave
+        # Marcador
+        global puntos
+        textoMarcador = fuenteMarcador.render(f'Puntos: {str(puntos)}',0,colorFuente)
+        ventana.blit(textoMarcador, (5,5))
         # Dibujamos Meteoritos cada cierto tiempo
         tiempo = time()
-        if tiempo - contador > 1:
+        if (tiempo - contador > 1) and (nave.vida == True):
             contador = tiempo
             posX = randint(10, 380)
             posY = -20
@@ -52,6 +80,12 @@ def meteoritos():
                 asteroide.recorrido()
                 if asteroide.rect.top > 800:
                     listaAsteroides.remove(asteroide)
+                else:
+                    if asteroide.rect.colliderect(nave.rect):
+                        listaAsteroides.remove(asteroide)
+                        sonidoColision.play()
+                        nave.vida = False
+                        gameOver()
 
         # Disparos/proyectiles
         if len(nave.listaDisparo) > 0:
@@ -60,19 +94,31 @@ def meteoritos():
                 disparo.recorrido()
                 if disparo.rect.top < -10:
                     nave.listaDisparo.remove(disparo)
+                else:
+                    for asteroide in listaAsteroides:
+                        if disparo.rect.colliderect(asteroide):
+                            listaAsteroides.remove(asteroide)
+                            nave.listaDisparo.remove(disparo)
+                            sonidoDestruccion.play()
+                            puntos += 1
         nave.mover()
         for evento in pygame.event.get():
             if evento.type == QUIT:
                 pygame.quit()
                 sys.exit()
             elif evento.type == pygame.KEYDOWN:
-                if evento.key == K_LEFT:
-                    nave.rect.left -= nave.velocidad
-                elif evento.key == K_RIGHT:
-                    nave.rect.right += nave.velocidad
-                elif evento.key == K_SPACE:
-                    x, y = nave.rect.center
-                    nave.disparar(x, y)
+                if nave.vida == True:
+                    if evento.key == K_LEFT:
+                        nave.rect.left -= nave.velocidad
+                    elif evento.key == K_RIGHT:
+                        nave.rect.right += nave.velocidad
+                    elif evento.key == K_SPACE:
+                        x, y = nave.rect.center
+                        nave.disparar(x, y)
+        if jugando == False:
+            textoGameOver = fuenteGameOver.render("Game Over", 0, colorFuente)
+            ventana.blit(textoGameOver, (100, 300))
+            pygame.mixer.music.fadeout(3000)
         pygame.display.update()
 
 
